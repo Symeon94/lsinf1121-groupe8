8c514ac4654d356ccdf8c5291fb9fd3c1cb02535
Symeon94 <symeon94@hotmail.com>
2013-09-27 14:57:53 +0200

    Correction Token
    Ajout de l'import java.util et j'ai viré le T majuscule de Hashtable

--- projet1-lsinf1121/src/Token.java
+++ projet1-lsinf1121/src/Token.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 abstract public class Token
 {
-	private static HashTable<String, Operator> operations;
+	private static Hashtable<String, Operator> operations;
 	
 	public static void init()
 	{
-		operations = new HashTable<String,Operator>();
+		operations = new Hashtable<String,Operator>();
 		//operations.put("add", new Add());
 		// TODO ADD OPERATIONS
 	}
